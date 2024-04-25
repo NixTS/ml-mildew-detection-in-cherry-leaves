edee23f92c6c0e9077e146cd409a2a027ce958ba
TobiS S <tobi.schmauder@outlook.com>
2024-04-25 17:24:15 +0000

    Add app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from app_pages.multipage import MultiPage
+
+# load pages scripts
+from app_pages.page_summary import page_summary_body
+from app_pages.page_leaves_visualizer import page_leaves_visualizer_body
+from app_pages.page_powdery_mildew_detector import page_powdery_mildew_detector_body
+from app_pages.page_project_hypothesis import page_project_hypothesis_body
+from app_pages.page_ml_performance import page_ml_performance_metrics
+
+# Create an instance of the app
+app = MultiPage(app_name="Powdery Mildew Detector")  
+
+# Add your app pages here using .add_page()
+app.add_page("Quick Project Summary", page_summary_body)
+app.add_page("Leaves Visualiser", page_leaves_visualizer_body)
+app.add_page("Powdery Mildew detector", page_powdery_mildew_detector_body)
+app.add_page("Project Hypothesis", page_project_hypothesis_body)
+app.add_page("ML Performance Metrics", page_ml_performance_metrics)
+
+app.run()  # Run the app
