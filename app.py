8491b22df222b1d0576083995e9357d0513f1c7c
TobiS S <tobi.schmauder@outlook.com>
2024-04-26 17:12:22 +0000

    Add project hypothesis the pages

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from app_pages.multipage import MultiPage
 from app_pages.page_summary import page_summary_body
 from app_pages.page_leaves_visualizer import page_leaves_visualizer_body
 from app_pages.page_powdery_mildew_detector import page_powdery_mildew_detector_body
-# from app_pages.page_project_hypothesis import page_project_hypothesis_body
+from app_pages.page_project_hypothesis import page_project_hypothesis_body
 # from app_pages.page_ml_performance import page_ml_performance_metrics
 
 # Create an instance of the app
@@ -15,7 +15,7 @@ app = MultiPage(app_name="Powdery Mildew Detector")
 app.add_page("Quick Project Summary", page_summary_body)
 app.add_page("Leaves Visualiser", page_leaves_visualizer_body)
 app.add_page("Powdery Mildew detector", page_powdery_mildew_detector_body)
-# app.add_page("Project Hypothesis", page_project_hypothesis_body)
+app.add_page("Project Hypothesis", page_project_hypothesis_body)
 # app.add_page("ML Performance Metrics", page_ml_performance_metrics)
 
 app.run()  # Run the app
---
+++ app_pages/page_project_hypothesis.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+
+
+def page_project_hypothesis_body():
+    st.write("### Project Hypotesis 1 and Validation")
+
+    st.info(
+        f"Infected leaves are visually different from healthy ones."
+    )
+
+    st.warning(
+        f"Healthy cherry leaves are characterized by their rich, dark green color, "
+        f"which often appear vibrant and glossy. \n\n"
+        f"Powdery mildew on cherry leaves typically appears as a white to grayish powdery "
+        f"growth on the surface of infected plant tissues."
+    )
+
+    st.success(
+        f"Through intensive training and fine tuning the model was able to detect such differences, "
+        f"with an accuracy of more than 99%. \n"
+        f"Testing the model with unfamiliar data confirmes this very favourable outcome. "
+        f"This validates, that the model is able to generalize and predict reliably."
+    )
+
+    st.write(
+        f"For more information, please visit the "
+        f"[**Project README.**](https://github.com/NixTS/ml-mildew-detection-in-cherry-leaves/blob/main/README.md)")
